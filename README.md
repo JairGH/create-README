70f4c6c0387431124d9c04aa6357ef00998c2bea
Jair Gomez <jairherber505@gmail.com>
2023-05-15 15:22:13 -0700

    nice progress

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# CreateREADME
+# create readme
+    
+## Description
+
+it will create a readme file for the user
+
+- What was your motivation? hw
+- Why did you build this project? hw
+- What problem does it solve? it gives a profesional readme structure
+- What did you learn? node js
+
+## Installation
+
+npm something something
+
+## Usage 
+
+nope
+
+//How can i add images?
+
+## Credits
+
+no ty
+    
--- index.js
+++ index.js
@@ -1,32 +1,95 @@
-var prompt = require("prompt");
+const inquirer = require("inquirer");
 const fs = require("fs");
-const { message } = require("prompt");
-const { title } = require("process");
 
-const generateREADME = ({ title, description }) =>
+const generateREADME = ({
+  title,
+  description,
+  firstQ,
+  secondQ,
+  thirdQ,
+  fourthQ,
+  installation,
+  usage,
+  credits
+}) =>
   `# ${title}
     
-    ## Description
+## Description
 
-    ${description}
+${description}
 
-    - What was your motivation?
-    
+- What was your motivation? ${firstQ}
+- Why did you build this project? ${secondQ}
+- What problem does it solve? ${thirdQ}
+- What did you learn? ${fourthQ}
+
+## Installation
+
+${installation}
+
+## Usage 
+
+${usage}
+
+//How can i add images?
+
+## Credits
+
+${credits}
     `;
 
-inquirer.prompt([
-  {
-    type: "input",
-    name: "title",
-    message: "What is the title of your project?",
-  },
-  {
-    type: "input",
-    name: "description",
-    message: "Short description:",
-  },
-]);
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of your project?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Short description:",
+    },
+    {
+      type: "input",
+      name: "firstQ",
+      message: "What was your motivation?",
+    },
+    {
+      type: "input",
+      name: "secondQ",
+      message: "Why did you build this project?",
+    },
+    {
+      type: "input",
+      name: "thirdQ",
+      message: "What problem does it solve?",
+    },
+    {
+      type: "input",
+      name: "fourthQ",
+      message: "What did you learn?",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "What are the steps required to install your project?",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Provide instructions and examples for use.",
+    },
+    {
+      type: "input",
+      name: "credits",
+      message: "Provide collaborators, third-party assets, or followed tutorials.",
+    },
+  ])
+  .then((answers) => {
+    const readmePageContent = generateREADME(answers);
 
-fs.writeFile("readme.md", "utf8", (error, data) =>
-  error ? console.error(error) : console.log(data)
-);
+    fs.writeFile("readme.md", readmePageContent, (err) =>
+      err ? console.error(err) : console.log("Success!")
+    );
+  });
